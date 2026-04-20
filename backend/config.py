d383dd5491e8b01940bfaa841752b1630d45edb3
Nate Kelly <njk3lly@gmail.com>
2026-04-20 22:06:01 +0200

    Update monitored services for EC2 deployment

--- backend/config.py
+++ backend/config.py
@@ -1,22 +1,6 @@
 SERVICES = [
-    {
-        "name": "Google",
-        "type": "http",
-        "url": "https://www.google.com"
-    },
-    {
-        "name": "Cloudflare DNS",
-        "type": "http",
-        "url": "https://1.1.1.1"
-    },
-    {
-        "name": "Lab PC",
-        "type": "ping",
-        "host": "192.168.2.123"
-    },
-    {
-        "name": "Ubuntu Server",
-        "type": "ping",
-        "host": "127.0.0.1"
-    }
+    {"name": "Google", "type": "http", "url": "https://www.google.com"},
+    {"name": "Cloudflare DNS", "type": "http", "url": "https://1.1.1.1"},
+    {"name": "LabWatch API", "type": "http", "url": "http://localhost:8080"},
+    {"name": "auxcon.dev", "type": "http", "url": "https://api.auxcon.dev"},
 ]
